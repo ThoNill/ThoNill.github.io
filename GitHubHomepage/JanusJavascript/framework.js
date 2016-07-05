8da7dca92d65b5b8fdc7ec357b279df60894bb53
Thomas Nill <t.nill@t-online.de>
2016-07-05 05:43:40 +0200

    Übernommen aus JanusJavascript

--- GitHubHomepage/JanusJavascript/framework.js
+++ GitHubHomepage/JanusJavascript/framework.js
@@ -1170,7 +1170,7 @@ var JanusJS = (function() {
 			"DATEFIELD",
 			{
 //				DATEFIELD : "<div  id='${id}' ${styleOut}  >${startTooltip}<input   id='ip${id}' ${styleOut}  class='input-control text margin10 no-margin-right ' type='date' name='${model}' value='${value}' onkeypress=\"return JanusJS.setElementValueEnter(event,'${id}',this.value);\" />${endTooltip}</div>"
-				DATEFIELD : "<div  id='${id}' >${startTooltip}<div ${styleOut} data-role='datepicker' data-format='dd.mm.yyyy' ><input   id='ip${id}' ${styleOut}  class='input-control text margin10 no-margin-right ' type='text' name='${model}' value='${value}' onkeypress=\"return JanusJS.setElementValueEnter(event,'${id}',this.value);\" /> <button class='button'><span class='mif-calendar'></span></button>${endTooltip}</div></div>"
+				DATEFIELD : "<div  id='${id}' class='margin10 no-margin-right' >${startTooltip}<div ${styleOut}  class='input-control text' data-role='datepicker' data-format='dd.mm.yyyy' ><input   id='ip${id}' ${styleOut}  type='text' name='${model}' value='${value}' onkeypress=\"return JanusJS.setElementValueEnter(event,'${id}',this.value);\" /> <button class='button'><span class='mif-calendar'></span></button>${endTooltip}</div></div>"
 
 			});
 	guiTag.DATEFIELD.fill = simpleFill;
